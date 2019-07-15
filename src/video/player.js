64b8fd8320c743b59b74a1f0ba1893d6c474c79d
Jiabo.Wu <wujbow@cn.ibm.com>
2019-07-15 14:52:34 +0800

    init

--- src/video/player.js
+++ src/video/player.js
@@ -4,11 +4,51 @@ export default class Player extends React.Component {
 
     render() {
         return (
-            <div>{this.props.ctrl}
-                <video src={this.props.url}>
+            <div>
+                <video id='media' ref='media' src={this.props.url}>
                     Your browser does not support the video tag.
                 </video>
             </div>
+
         );
+    }
+    componentDidMount() {
+    }
+
+    componentWillReceiveProps(newProps) {
+        let video = this.refs.media;
+        if (newProps.ctrl === 'play' && this.props.url) {
+            video.play();
+        }
+        if (newProps.ctrl === 'pause' && this.props.url) {
+            video.pause();
+        }
+        if (newProps.ctrl === 'plus' && this.props.url) {
+            if (video.volume <= 0.9) {
+                video.volume = video.volume + 0.1;
+            }
+        }
+        if (newProps.ctrl === 'minus' && this.props.url) {
+            if (video.volume >= 0.1) {
+                video.volume = video.volume - 0.1;
+            }
+        }
+        if (newProps.ctrl === 'refresh' && this.props.url) {
+            video.load();
+            video.play();
+        }
+        if (newProps.ctrl === 'muted' && this.props.url) {
+            if (video.muted) {
+                video.muted = false;
+            } else {
+                video.muted = true;
+            }
+        }
+        if (newProps.ctrl === 'thumbsUp' && this.props.url) {
+            
+        }
+        if (newProps.ctrl === 'thumbsDown' && this.props.url) {
+            
+        }
     }
 }
